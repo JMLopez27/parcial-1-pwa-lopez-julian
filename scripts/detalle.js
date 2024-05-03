aa5e3224e1a549d7616bd2a8f9b3e6aaea67e729
JMLopez27 <julian.lopez27@hotmail.com>
2024-05-03 01:10:32 -0300

    feat: Se añidio una variable para poder hacer un fondo gradiante dependiendo del tipo de pokemon que se trate

--- scripts/detalle.js
+++ scripts/detalle.js
@@ -28,6 +28,15 @@ function crearCartaDetalle(pokemonDatos) {
     divCarta.classList.add('carta');
     main.appendChild(divCarta);
 
+    const tipos = pokemonDatos.types.map(tipo => tipo.type.name);
+
+    let backgrounds = tipos.map(type => fondosXtipos[type]);
+    backgrounds = backgrounds.filter(bg => bg !== undefined);
+  
+    if (backgrounds.length > 0) {
+      divCarta.style.background = backgrounds.join(', ');
+    };
+
     const divH2 = document.createElement('div')
     divH2.classList.add('tituloCarta');
     divCarta.appendChild(divH2);
@@ -64,6 +73,10 @@ function crearCartaDetalle(pokemonDatos) {
     const divDetalle = document.createElement('div');
     divDetalle.classList.add('detalle');
     main.appendChild(divDetalle);
+
+    if (backgrounds.length > 0){
+        divDetalle.style.background = backgrounds.join(', ');
+    };
     
     const h3Datos = document.createElement('h3');
     h3Datos.innerText = `Estadisticas ${pokemonDatos.name.charAt(0).toUpperCase() + pokemonDatos.name.slice(1)}`;
@@ -78,11 +91,11 @@ function crearCartaDetalle(pokemonDatos) {
     divEstadisticas.appendChild(divPesoAltura);
 
     const peso = document.createElement('p');
-    peso.innerText = `Peso: ${pokemonDatos.weight}`;
+    peso.innerText = `Peso: ${pokemonDatos.weight}kg`;
     divPesoAltura.appendChild(peso);
 
     const altura = document.createElement('p');
-    altura.innerText = `Altura: ${pokemonDatos.height}`;
+    altura.innerText = `Altura: ${pokemonDatos.height}cm`;
     divPesoAltura.appendChild(altura);
 
     const divStatsBase = document.createElement('div');
@@ -95,3 +108,32 @@ function crearCartaDetalle(pokemonDatos) {
         divStatsBase.appendChild(pStats);
     })
 }
+
+const fondosXtipos = {
+    "fire-flying": "linear-gradient(to bottom right, rgb(255, 60, 0), rgba(151,167,241,255))",
+    "grass-poison": "linear-gradient(to bottom right, rgba(140,198,94,255), rgba(141,70,146,255))",
+    "bug-poison": "linear-gradient(to bottom right, rgba(169,184,57,255), rgba(141,70,146,255))",
+    "bug": "linear-gradient(to bottom right, rgba(169,184,57,255), rgba(93,156,46,255))",
+    "fire": "linear-gradient(to bottom right, red, orange)",
+    "water": "linear-gradient(to bottom right, rgba(57,142,226,255), rgba(145,200,245,255))",
+    "normal-flying": "linear-gradient(to bottom right, rgba(195,193,186,255), rgba(151,167,241,255))",
+    "bug-flying": "linear-gradient(to bottom right, rgba(169,184,57,255), rgba(151,167,241,255))",
+    "normal": "linear-gradient(to bottom right, rgba(195,193,186,255), gray)",
+    "poison": "linear-gradient(to bottom right, rgba(141,70,146,255), violet)",
+    "electric": "linear-gradient(to bottom right, rgba(249,181,39,255), orange)",
+    "water-ice": "linear-gradient(to bottom right, rgba(57,142,226,255), rgba(115,212,243,255))",
+    "poison-flying": "linear-gradient(to bottom right, rgba(141,70,146,255), rgba(151,167,241,255))",
+    "bug-glass": "linear-gradient(to bottom right, rgba(169,184,57,255), rgba(140,198,94,255))",
+    "ground": "linear-gradient(to bottom right, rgba(203,176,94,255), rgb(134, 71, 71))",
+    "poison-ground": "linear-gradient(to bottom right, rgba(141,70,146,255), rgba(203,176,94,255))",
+    "water-fighting": "linear-gradient(to bottom right, rgba(57,142,226,255), brown)",
+    "fairy": "linear-gradient(to bottom right, rgba(237,193,237,255), violet)",
+    "psychic": "linear-gradient(to bottom right, rgba(229,74,127,255), rgb(212, 5, 74))",
+    "fighting": "linear-gradient(to bottom right, rgb(226, 9, 9), rgba(120,53,33,255))",
+    "normal-fairy": "linear-gradient(to bottom right, rgba(195,193,186,255), rgba(237,193,237,255))",
+    "water-poison": "linear-gradient(to bottom right, rgba(57,142,226,255), rgba(141,70,146,255))",
+    "rock-ground": "linear-gradient(to bottom right, rgba(133,113,68,255), rgba(203,176,94,255))",
+    "electric-steel": "linear-gradient(to bottom right, rgba(249,181,39,255), rgba(197,198,210,255))",
+    "water-psychic": "linear-gradient(to bottom right, rgba(57,142,226,255), rgba(229,74,127,255))",
+    "ghost-poison": "linear-gradient(to bottom right, rgba(80,63,48,255), rgba(141,70,146,255))",
+};
