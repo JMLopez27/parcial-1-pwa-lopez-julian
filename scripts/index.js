6c85da52493fbccc1b9ad27ef1720e1fce86c295
JMLopez27 <julian.lopez27@hotmail.com>
2024-05-04 18:20:55 -0300

    feat: Se agrego la funcion para el campo de busqueda y una variable pokemondatos que es un array y pushear todos los datos a dicho array

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 const URL_endpoint = 'https://pokeapi.co/api/v2/';
 const URL_pokemones = URL_endpoint + 'pokemon?limit=100&offset=0';
 
+let pokemonDatos = [];
+
 fetch(URL_pokemones)
     .then(response => response.json())
     .then(pokemonData => {
@@ -9,15 +11,16 @@ fetch(URL_pokemones)
 
             fetch(pokemonURL)
                 .then(response => response.json())
-                .then(pokemonDatos =>{
-                    crearCard(pokemonDatos);
+                .then(pokemonData => {
+                    pokemonDatos.push(pokemonData);
+                    crearCard(pokemonData);
                 })
         });
     })
 
-function crearCard(pokemonDatos){
+function crearCard(pokemonData) {
     const contenedor = document.getElementById("contenidoPrincipal");
-    
+
     const card = document.createElement('div');
     card.classList.add('card');
 
@@ -26,16 +29,16 @@ function crearCard(pokemonDatos){
     card.appendChild(divH2);
 
     const h2 = document.createElement('h2');
-    h2.innerText = pokemonDatos.name.charAt(0).toUpperCase() + pokemonDatos.name.slice(1);
+    h2.innerText = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
     divH2.appendChild(h2);
 
     const img = document.createElement('img');
-    img.src = pokemonDatos.sprites.other.dream_world.front_default;
-    img.alt = pokemonDatos.name;
+    img.src = pokemonData.sprites.other.dream_world.front_default;
+    img.alt = pokemonData.name;
     divH2.appendChild(img);
 
     const p = document.createElement('p');
-    p.innerText = pokemonDatos.types.map(tipos => tipos.type.name.toUpperCase());
+    p.innerText = pokemonData.types.map(tipos => tipos.type.name.toUpperCase());
     divH2.appendChild(p);
 
     const divBoton = document.createElement('div');
@@ -44,14 +47,31 @@ function crearCard(pokemonDatos){
 
     const boton = document.createElement('button');
     boton.innerText = 'Detalle';
-    boton.addEventListener('click', () =>{
-        mostrarDetalle(pokemonDatos.id);
+    boton.addEventListener('click', () => {
+        mostrarDetalle(pokemonData.id);
     })
     divBoton.appendChild(boton);
 
     contenedor.appendChild(card);
 }
 
-function mostrarDetalle(pokemonId){
+function mostrarDetalle(pokemonId) {
     window.location.href = `http://127.0.0.1:5500/detalle.html?id=${pokemonId}`;
+}
+const buscador = document.getElementById("buscador")
+buscador.addEventListener("input", buscarPokemon);
+
+function buscarPokemon() {
+    const valorBuscador = buscador.value.toLowerCase();
+
+    const filtroPokemon = pokemonDatos.filter(pokemon =>
+        pokemon.name.toLowerCase().includes(valorBuscador)
+    );
+
+    const contenedor = document.getElementById("contenidoPrincipal");
+    contenedor.innerHTML = "";
+
+    filtroPokemon.forEach(pokemon => {
+        crearCard(pokemon);
+    });
 }
